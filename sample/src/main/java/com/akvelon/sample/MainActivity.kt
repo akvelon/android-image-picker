de59b90eb6258164e83c9142da13ee1e451b5f77
rustem.muzafarov <klvra.05@gmail.com>
2020-05-29 14:45:34 +0300

    sample updated

--- sample/src/main/java/com/akvelon/sample/MainActivity.kt
+++ sample/src/main/java/com/akvelon/sample/MainActivity.kt
@@ -4,19 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.akvelon.imagepicker.ImagePicker
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        ImagePicker.launch(this)
+        button.setOnClickListener { ImagePicker.launch(this) }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if(ImagePicker.shouldResolve(requestCode, resultCode)) {
-            val result = ImagePicker.getImages(data)
+            val result = ImagePicker.getSingleImageOrNull(data)
+            result?.let {
+                Glide.with(this)
+                    .load(result)
+                    .into(imageView)
+            }
         } else super.onActivityResult(requestCode, resultCode, data)
     }
 }
